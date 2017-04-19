08dc047ce0a6269e963ea68d224ebbf321880395
alexdotterweich <alexdotterweich@berkeley.edu>
2017-04-19 13:56:16 -0700

    Added Test to repo

---
+++ src/TicTacToe.java
@@ -0,0 +1,5 @@
+
+
+//Understands how to play tic tac toe with a user on the command line
+public class TicTacToe {
+}
---
+++ test/TicTacToeTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+
+public class TicTacToeTest {
+}
