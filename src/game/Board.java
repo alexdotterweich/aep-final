c7191df2a3975868e6b9c7af50a4b42bbbb77515
alexdotterweich <alexdotterweich@berkeley.edu>
2017-04-21 13:14:14 -0700

    Made ant do stuff properly

--- src/game/Board.java
+++ src/game/Board.java
@@ -11,7 +11,6 @@ public class Board {
         this.board = Arrays.asList(" ", " ", " ", " ", " ", " ", " ", " ", " ");
     }
 
-
     public List getBoard() {
         return board;
     }
--- test/game/AITest.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-/**
- * Created by alexdotterweich on 4/19/17.
- */
-public class AITest {
-}
